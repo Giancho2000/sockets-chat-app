5832888553639a7062717faa34e0566f29a16590
Giancarlo Reina <giancarloreina125@gmail.com>
2025-02-09 23:18:30 -0500

    Se agrego el frontend y se añadio la renderizacion de los usuarios activos

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -4,10 +4,10 @@ var params = new URLSearchParams(window.location.search);
 
 if (!params.has("nombre") || !params.has("sala")) {
   window.location = "index.html";
-  throw new Error("El nombre o sala son necesarios");
+  throw new Error("El nombre y sala son necesarios");
 }
 
-let usuario = {
+var usuario = {
   nombre: params.get("nombre"),
   sala: params.get("sala"),
 };
@@ -16,7 +16,8 @@ socket.on("connect", function () {
   console.log("Conectado al servidor");
 
   socket.emit("entrarChat", usuario, function (resp) {
-    console.log("Usuarios conectados", resp);
+    //console.log('Usuarios conectados', resp);
+    renderizarUsuarios(resp);
   });
 });
 
@@ -26,29 +27,25 @@ socket.on("disconnect", function () {
 });
 
 // Enviar información
-/* socket.emit(
-  "enviarMensaje",
-  {
-    usuario: "Fernando",
-    mensaje: "Hola Mundo",
-  },
-  function (resp) {
-    console.log("respuesta server: ", resp);
-  }
-); */
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
-socket.on("enviarMensaje", function (mensaje) {
+socket.on("crearMensaje", function (mensaje) {
   console.log("Servidor:", mensaje);
 });
 
-// Escuchar cambios de usuario (Entra o sale del chat) listaPersonas
-socket.on("listaPersonas", function (personas) {
-  console.log(personas);
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
+socket.on("listaPersona", function (personas) {
+  renderizarUsuarios(resp);
 });
 
-// Mensaje privado
-
+// Mensajes privados
 socket.on("mensajePrivado", function (mensaje) {
-  console.log("mensaje Privado", mensaje);
+  console.log("Mensaje Privado:", mensaje);
 });
